200c42e8f8751449a0796d475809c0ad5c13e7e4
¨Nwachukwu-Uzor¨ <uzor1997@gmail.com>
2025-01-28 21:51:53 +0100

    implement: hero section and navbar on the links shortner landing page

---
+++ 6-exams/scripts.js
@@ -0,0 +1,25 @@
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("loaded...");
+
+  const toggleNavBtn = document.querySelector("#toggleMenu");
+  const toggleIcon = document.querySelector("#openMenuBtn");
+  const navMenuContainer = document.querySelector("#navMenuContainer");
+
+  toggleNavBtn.addEventListener("click", () => {
+    const openIconClass = "fa-bars";
+    const closeIconClass = "fa-xmark";
+    const closeMenuClass = "close";
+    const openMenuClass = "open";
+    if (toggleIcon.classList.contains(openIconClass)) {
+      toggleIcon.classList.remove(openIconClass);
+      toggleIcon.classList.add(closeIconClass);
+      navMenuContainer.classList.remove(closeMenuClass);
+      navMenuContainer.classList.add(openMenuClass);
+    } else {
+      toggleIcon.classList.remove(closeIconClass);
+      toggleIcon.classList.add(openIconClass);
+      navMenuContainer.classList.add(closeMenuClass);
+      navMenuContainer.classList.remove(openMenuClass);
+    }
+  });
+});
